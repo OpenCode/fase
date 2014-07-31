4d64cba9d00033e791315e7ffd2a1f3b16724fc8
Francesco Apruzzese <cescoap@gmail.com>
2014-07-31 10:11:13 +0200

    [IMP] Added the possibility to pass login info to software and autologin

--- fase.py
+++ fase.py
@@ -24,31 +24,63 @@
 # ######################################################################
 
 import sys
+import argparse
 from PyQt4 import QtGui, QtCore, QtWebKit
+
+jsFillLoginForm = """ 
+    document.getElementById('email').value='{username}';
+    document.getElementById('pass').value='{password}';
+    """
+jsClickLoginButton = """
+    document.getElementById('u_0_l').click(); void(0);
+    """
 
 
 class Fase(QtGui.QMainWindow):
 
-    def __init__(self):
+    def _loadFinished(self):
+        if self.args.username and self.args.password:
+            self.web.page().mainFrame().evaluateJavaScript(
+                jsFillLoginForm.format(username = self.args.username,
+                                       password = self.args.password)
+                )
+            if self.args.auto_login:
+                self.web.page().mainFrame().evaluateJavaScript(
+                    jsClickLoginButton)
+
+    def __init__(self, args):
+        self.args = args
         # ----- Window
         QtGui.QMainWindow.__init__(self)
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.resize(800, 600)
         self.setWindowState(QtCore.Qt.WindowMaximized)
         self.setWindowTitle('FASE - Facebook Separated Environment')
-        self.statusBar().showMessage(
-            'Welcome to FaSE')
         # ----- Content creation
         main_grid = QtGui.QGridLayout()
         self.main_grid = main_grid
         # ----- Webkit
-        web = QtWebKit.QWebView()
-        web.setUrl(QtCore.QUrl('http://www.facebook.com'))
-        self.setCentralWidget(web)
+        self.web = QtWebKit.QWebView()
+        self.web.setUrl(QtCore.QUrl('http://www.facebook.com'))
+        self.web.loadFinished.connect(self._loadFinished)
+        self.setCentralWidget(self.web)
 
 
 if __name__ == "__main__":
+
+    # ----- Parse terminal arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username', dest='username',
+                        help='Set username')
+    parser.add_argument('-p', '--password', dest='password',
+                        help='Set password')
+    parser.add_argument('-l', '--auto-login', dest='auto_login',
+                        action='store_true',
+                        help='Autologin in Facebook')
+    args = parser.parse_args()
+
+    # ----- Init app
     app = QtGui.QApplication(sys.argv)
-    fase_main = Fase()
+    fase_main = Fase(args)
     fase_main.show()
     sys.exit(app.exec_())
